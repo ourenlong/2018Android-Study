9c4015c417443ecbe169d24a8773f29b872ad526
ourenlong <712346172@qq.com>
2018-04-25 19:48:24 +0800

    git tracks changes

---
+++ First_Term/ourenlong/work1/ActivityTest/app/src/main/java/com/example/a15541/activitytest/FirstActivity.java
@@ -0,0 +1,52 @@
+package com.example.a15541.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button_l);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(FirstActivity.this,"you clicked Button1",
+                        Toast.LENGTH_SHORT).show();
+                //隐式
+                Intent intent=new Intent("com.example.activitytest.ACTION_START");
+                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+
+                //显式  Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+    public boolean onCcrateOpyionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"you clicked remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return  true;
+    }
+}
